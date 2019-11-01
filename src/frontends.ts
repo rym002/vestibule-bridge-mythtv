8072f76595d1cb3ef103f6c8312971c7165db357
rym002 <raymond.munian@gmail.com>
2019-11-01 19:59:16 -0400

    Monitor Frontend connection

--- src/frontends.ts
+++ src/frontends.ts
@@ -3,13 +3,19 @@ import { mythNotifier, MythSenderEventEmitter } from "mythtv-event-emitter";
 import { ApiTypes, Frontend, getFrontendServices } from "mythtv-services-api";
 import { mergeObject } from "./mergeObject";
 import { EventMapping } from 'mythtv-event-emitter/dist/messages';
-
+import { EndpointEmitter } from '@vestibule-link/bridge-assistant'
+import { AssistantType } from '@vestibule-link/iot-types'
 export const frontends: MythEventFrontend[] = []
 
+const FE_POLLING_INTERVAL = Number(process.env['MYTH_FE_POLLING_INTERVAL'] || 30000)
+type Callback = () => void;
 export class CachingEventFrontend {
     private readonly memoizeStatus: MemoizedFunction
     private readonly memoizeEventDelta: MemoizedFunction
-
+    private assistantEmitters = new Map<AssistantType, EndpointEmitter<any>>()
+    private disconnectCallbacks = new Set<Callback>()
+    private connected = true
+    private fePollingTimeout?: NodeJS.Timeout
     GetStatus: () => Promise<ApiTypes.FrontendStatus>
     eventDeltaId: () => symbol
     constructor(private readonly fe: Frontend.Service, readonly mythEventEmitter: MythSenderEventEmitter) {
@@ -25,6 +31,64 @@ export class CachingEventFrontend {
             this.clearStatusCache()
             this.clearEventDeltaCache()
         });
+        mythEventEmitter.on('CLIENT_CONNECTED', message => {
+            this.connected = true
+            this.pollConnection()
+            this.refreshEmitters()
+        })
+        mythEventEmitter.on('CLIENT_DISCONNECTED', message => {
+            this.connected = false
+            this.clearPollConnection()
+            this.executeDisconnect()
+        })
+    }
+    public addConnectionMonitor<T extends AssistantType>(assistantType: T, emitter: EndpointEmitter<T>, disconnectCallback: Callback) {
+        this.assistantEmitters.set(assistantType, emitter)
+        this.disconnectCallbacks.add(disconnectCallback)
+        if (!this.fePollingTimeout) {
+            this.pollConnection()
+        }
+    }
+    public removeConnectionMonitor<T extends AssistantType>(assistantType: T, disconnectCallback: Callback) {
+        this.assistantEmitters.delete(assistantType)
+        this.disconnectCallbacks.delete(disconnectCallback)
+        if (this.assistantEmitters.size == 0) {
+            this.clearPollConnection()
+        }
+    }
+    private pollConnection() {
+        if (this.connected
+            && FE_POLLING_INTERVAL > 0
+            && !this.fePollingTimeout) {
+            this.fePollingTimeout = setInterval(() => {
+                this.fe.GetStatus()
+                    .then(() => {
+                        this.connected = true
+                    }).catch(err => {
+                        console.log(err)
+                        this.connected = false
+                        this.clearPollConnection()
+                        this.executeDisconnect()
+                    })
+            }, FE_POLLING_INTERVAL);
+        }
+    }
+
+    private clearPollConnection() {
+        if (this.fePollingTimeout) {
+            clearInterval(this.fePollingTimeout)
+            this.fePollingTimeout = undefined
+        }
+    }
+    private refreshEmitters() {
+        this.assistantEmitters.forEach(emitter => {
+            emitter.refresh(this.eventDeltaId())
+        })
+    }
+    private executeDisconnect() {
+        this.disconnectCallbacks.forEach(callback => {
+            callback()
+        })
     }
     async isWatchingTv(): Promise<boolean> {
         const status: ApiTypes.FrontendStatus = await this.GetStatus();
@@ -92,6 +156,8 @@ export interface MythEventFrontend extends Frontend.Service {
     GetRefreshedStatus(): Promise<ApiTypes.FrontendStatus>
     eventDeltaId(): symbol
     monitorMythEvent<T extends keyof EventMapping, P extends EventMapping[T]>(eventName: T, timeout: number): Promise<P>;
+    addConnectionMonitor<T extends AssistantType>(assistantType: T, emitter: EndpointEmitter<T>, disconnectCallback: Callback): void;
+    removeConnectionMonitor<T extends AssistantType>(assistantType: T, disconnectCallback: Callback): void;
 }
 
 function initFrontend(fe: Frontend.Service): MythEventFrontend {
--- test/frontends.test.ts
+++ test/frontends.test.ts
@@ -1,9 +1,40 @@
 import 'mocha';
-import { frontends, loadFrontends } from '../src/frontends'
+import { frontends, loadFrontends, MythEventFrontend } from '../src/frontends'
 import { expect } from 'chai'
 import * as nock from 'nock';
+import { EndpointEmitter } from '@vestibule-link/bridge-assistant';
+import { EventEmitter } from 'events';
+import { AlexaEndpoint, SubType } from '@vestibule-link/iot-types';
+import { mythNotifier } from 'mythtv-event-emitter';
 
+type DoneHolder = {
+    done: Mocha.Done
+    eventType: 'CLIENT_CONNECTED' | 'CLIENT_DISCONNECTED'
+}
+class RefreshEmitter extends EventEmitter implements EndpointEmitter<'alexa'> {
+    endpoint: AlexaEndpoint = {}
+    readonly doneTrackers = new Map<symbol, DoneHolder>()
+    private markDone(deltaId: symbol, eventType: SubType<DoneHolder, 'eventType'>) {
+        const done = this.doneTrackers.get(deltaId)
+        if (done) {
+            if (done.eventType == eventType) {
+                done.done()
+            } else {
+                done.done('Unexpected Event Type: ' + eventType)
+            }
+        }
+    }
+    async refresh(deltaId: symbol) {
+        this.markDone(deltaId, 'CLIENT_CONNECTED')
+    }
+    disconnect(fe: MythEventFrontend) {
+        return () => {
+            this.markDone(fe.eventDeltaId(), 'CLIENT_DISCONNECTED')
+        }
+    }
+}
 describe('frontends', () => {
+    const refreshEmitter = new RefreshEmitter()
     before(async () => {
         nock("http://localhost:6544/Myth")
             .get('/GetFrontends')
@@ -109,5 +140,43 @@ describe('frontends', () => {
         const eventDelta1 = frontend.eventDeltaId();
         const eventDelta2 = frontend.eventDeltaId();
         expect(eventDelta1).eql(eventDelta2);
+    })
+    context('Monitor Event', () => {
+        before(() => {
+            const frontend = frontends[0];
+            frontend.addConnectionMonitor('alexa', refreshEmitter, refreshEmitter.disconnect(frontend))
+        })
+        after(() => {
+            const frontend = frontends[0];
+            frontend.removeConnectionMonitor('alexa', refreshEmitter.disconnect(frontend))
+        })
+        it('should refreshEmitter on CLIENT_CONNECTED', function (done) {
+            const frontend = frontends[0];
+            frontend.mythEventEmitter.once('pre', (eventType, message) => {
+                if (eventType == 'CLIENT_CONNECTED') {
+                    refreshEmitter.doneTrackers.set(frontend.eventDeltaId(), {
+                        done: done,
+                        eventType: 'CLIENT_CONNECTED'
+                    })
+                }
+            })
+            mythNotifier.emit('MythEvent', frontend.hostname(), 'CLIENT_CONNECTED', {
+                SENDER: ''
+            })
+        })
+        it('should refreshEmitter on CLIENT_DISCONNECTED', function (done) {
+            const frontend = frontends[0];
+            frontend.mythEventEmitter.once('pre', (eventType, message) => {
+                if (eventType == 'CLIENT_DISCONNECTED') {
+                    refreshEmitter.doneTrackers.set(frontend.eventDeltaId(), {
+                        done: done,
+                        eventType: 'CLIENT_DISCONNECTED'
+                    })
+                }
+            })
+            mythNotifier.emit('MythEvent', frontend.hostname(), 'CLIENT_DISCONNECTED', {
+                SENDER: ''
+            })
+        })
     })
 })
